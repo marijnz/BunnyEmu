75920c6cec80449e7bc8b2e8009c2e9aec5111b6
_Valkryst <symphonic8@mail.com>
2014-06-06 10:40:41 -0300

    Reorganized get & set methods. Changed a Logger.writeError() statement to System.out.println() statement.

--- src/bunnyEmu/main/entities/WorldObject.java
+++ src/bunnyEmu/main/entities/WorldObject.java
@@ -1,7 +1,6 @@
 package bunnyEmu.main.entities;
 
 import bunnyEmu.main.entities.packet.Packet;
-import bunnyEmu.main.utils.Logger;
 import bunnyEmu.main.utils.math.Vector4;
 import bunnyEmu.main.utils.update.UpdateData;
 
@@ -22,7 +21,7 @@ public abstract class WorldObject {
 	protected UpdateData data;
 	
 	public WorldObject() {
-		Logger.writeError("Created worldobject with GUID " + countGUID);
+		System.out.println("Created worldobject with GUID " + countGUID);
 		this.setGUID(countGUID++);
 		setPosition(1,1,1,1);
 		this.data = new UpdateData();
@@ -32,29 +31,30 @@ public abstract class WorldObject {
 		data.writeUpdateFields(p);
 	}
 	
-	public void setPosition(float x, float y, float z, int mapId) {
-		this.position = new Vector4(x, y, z);
-		this.mapID = mapId;
-	}
-	
+	////////////////////// Get Methods:
 	public int getMapID() {
 		return mapID;
-	}
-
-	public void setMapID(int mapID) {
-		this.mapID = mapID;
 	}
 	
 	public Vector4 getPosition(){
 		return position;
 	}
-	/**
-	 * @return The generated GUID for this object
-	 */
+	
+	/** @return The generated GUID for this object */
 	public long getGUID() {
 		return guid;
 	}
 
+	////////////////////// Set Methods:
+	public void setMapID(int mapID) {
+		this.mapID = mapID;
+	}
+	
+	public void setPosition(float x, float y, float z, int mapId) {
+		this.position = new Vector4(x, y, z);
+		this.mapID = mapId;
+	}
+	
 	public void setGUID(long guid) {
 		this.guid = guid;
 	}
